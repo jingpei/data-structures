d59290839bc566c5d580e13c9086cb266133ba1b
jingpei <jingjpei@gmail.com>
2015-08-19 21:56:06 -0700

    Tree, linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = new Node(value);
+    /* Update node.next on the current tail*/
+    if(list.head === null){
+      list.head = node;
+    } else {
+      list.tail.next = node;
+    }
+
+    /* Update list.tail to new node */
+    list.tail = node;
   };
 
   list.removeHead = function(){
+    var temp = list.head.value;
+    list.head = list.head.next;
+    return temp;
   };
 
   list.contains = function(target){
+    var checkNode = function(node){
+      if(node.value === target){
+        return true;
+      } else if(node.next === null) {
+        return false;
+      } else {
+        return checkNode(node.next);
+      }
+    }
+
+    return checkNode(list.head);
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,26 +1,37 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
+  _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
-
-  // your code here
-  newTree.children = null;  // fix me
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var childTree = new Tree(value);
+  this.children.push(childTree);
 
 };
 
 treeMethods.contains = function(target){
+  var doesContain = false;
 
+  var checkChildren = function(parent) {
+    if (parent.value === target) {
+      doesContain = true;
+    } else if (parent.children.length > 0) {
+      /* Recurse */
+      for (var i = 0; i < parent.children.length; i++) {
+        checkChildren(parent.children[i]);
+      }
+    }
+  }
+
+  checkChildren(this);
+  return doesContain;
 };
 
 
