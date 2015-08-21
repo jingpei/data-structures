02117e7f3a6f47fe3287cfa4f8f87ce26c9ed52d
jingpei <jingjpei@gmail.com>
2015-08-20 21:45:47 -0700

    Savant

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,28 @@
 var BinarySearchTree = function(value){
+  this.value = value;
+  this.left = null;
+  this.right = null;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  var newBST = new BinarySearchTree(value);
+  if (this.value > value) {
+    this.right = newBST;
+  }else {
+    this.left = newBST;
+  }
+};
+
+BinarySearchTree.prototype.contains = function(target) {
+  if (target === this.value) {
+    return true;
+  } else if (this.left === null && this.right === null) {
+    return false;
+  } else if(target > this.value) {
+    this.contains(this.right);
+  } else if(target < this.value) {
+    this.contains(this.left);
+  };
 };
 
 
